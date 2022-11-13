2eeab066ba41df553992cb1051f7ec0c751733ab
Kirill <kirillandreev8888@gmail.com>
2022-11-14 02:03:25 +0300

    a6b

--- src/app/components/title-card/title-card.component.ts
+++ src/app/components/title-card/title-card.component.ts
@@ -1,22 +1,56 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { TitleInfoWithId, TitleInfo } from 'src/app/common/interfaces/title.interface';
-
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import {
+  TitleInfoWithId,
+  TitleInfo,
+  VoteValue,
+  Vote,
+} from 'src/app/common/interfaces/title.interface';
+import { GlobalSharedService } from 'src/app/global.shared.service';
+@UntilDestroy()
 @Component({
   selector: 'titleCard',
   templateUrl: './title-card.component.html',
   styleUrls: ['./title-card.component.scss'],
 })
 export class TitleCardComponent implements OnInit {
-  @Input()  mode?: 'titles' | 'archive';
+  @Input() mode?: 'titles' | 'archive';
   @Input() title!: TitleInfoWithId;
-  @Input()  checked!: boolean;
+  @Input() checked!: boolean;
   @Output() checkedChange = new EventEmitter<boolean>();
-  constructor() {}
+  constructor(public globalSharedService: GlobalSharedService) {}
+  userVote?: VoteValue = this.getUserVote();
+  yesVotes: string[] = this.getVotes('yes');
+  okVotes: string[] = this.getVotes('ok');
+  noVotes: string[] = this.getVotes('no');
 
   ngOnInit(): void {}
 
-  onCheck(){
-    this.checked = !this.checked;
-    this.checkedChange.emit(this.checked)
+  //обработка голосов
+  /** выдать список пользователей, отдавших конкретный голос */
+  getVotes(voteValue: VoteValue): string[] {
+    return this.title?.votes
+      ? this.title.votes
+          .filter((v) => v.value == voteValue)
+          .map((v) =>
+            v.name == this.globalSharedService.currentUser.value
+              ? 'Вы'
+              : v.name,
+          )
+      : [];
   }
+  getUserVote(): VoteValue | undefined {
+    if (this.title?.votes)
+      for (let vote of this.title.votes)
+        if (vote.name == this.globalSharedService.currentUser.value)
+          return vote.value;
+    return undefined;
+  }
+
+  onCheck() {
+    this.checked = !this.checked;
+    this.checkedChange.emit(this.checked);
+  }
+
+  voteClick(vote: VoteValue) {}
 }
