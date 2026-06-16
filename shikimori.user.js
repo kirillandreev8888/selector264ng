b4c9fadb9d408dc0d245bf96c3e18e9a6f22266c
kirillandreev8888 <31769418+kirillandreev8888@users.noreply.github.com>
2026-06-16 15:22:49 +0300

    fix: Update shikimori.user.js

--- shikimori.user.js
+++ shikimori.user.js
@@ -5,42 +5,46 @@
 // @match       https://shikimori.io/*
 // @match       https://shikimori.me/*
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      uspdd
-// @description 19.11.2022, 18:51:05
+// @description 16.06.2026, 15:13:26
 // @downloadURL https://raw.githubusercontent.com/kirillandreev8888/selector264ng/master/shikimori.user.js
+// @run-at      document-start
 // ==/UserScript==
-window.addEventListener('load', function() {
-  const interval = setInterval(() => {
-    if (document.querySelector('.c-image .b-add_to_list')){
-      clearInterval(interval);
-      addButton();
-    }
-  }, 100);
-}, false);
 
-const addButton = () => {
-  document.querySelector('.c-image .b-add_to_list').insertAdjacentHTML(
-    'beforeend',
-    `
-      <br>
-      <a style="margin-top: 0.5em !important" href="http://selector264.web.app/add?parseFromShikimori=${window.location.href}">
+(function() {
+  'use strict';
+
+  const addButton = (target) => {
+    if (target.querySelector('.my-custom-selector-btn')) return;
+
+    const cleanUrl = encodeURIComponent(window.location.href);
+
+    target.insertAdjacentHTML(
+      'beforeend',
+      `
+      <br class="my-custom-selector-btn">
+      <a class="my-custom-selector-btn" style="margin-top: 0.5em !important; display: block;" target="_blank" href="http://selector264.web.app/add?parseFromShikimori=${cleanUrl}">
           <div class="trigger">
-              <div class="text add-trigger"
-                   data-status="planned">
-                  <img height="25px"
-                       width="25px"
-                       src="https://selector264.web.app/assets/logo.svg"
-                       style="
-                      position: absolute;
-                  "><span class="status-name"
-                        data-text="Добавить в список"
-                        style="
-                      padding-left: 30px;
-                  "></span>
+              <div class="text add-trigger" data-status="planned">
+                  <img height="25px" width="25px" src="https://selector264.web.app/assets/logo.svg" style="position: absolute;">
+                  <span class="status-name" data-text="Добавить в список" style="padding-left: 30px;"></span>
               </div>
           </div>
       </a>
-      `,
-  );
-}
+      `
+    );
+  };
+
+  const observer = new MutationObserver(() => {
+    const target = document.querySelector('.c-image .b-add_to_list');
+    if (target) {
+      addButton(target);
+    }
+  });
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+})();
